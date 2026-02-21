866eb18310216480eeb33f84ec4f444041473dbc
Oleg Freedom <olegfreedom777@gmail.com>
2026-02-21 20:28:37 +0100

    Debug

--- app/Services/MessageParser.php
+++ app/Services/MessageParser.php
@@ -454,7 +454,26 @@ class MessageParser
         }
 
         $text      = str_replace("\r\n", "\n", $text);
+
+        // --- DEBUG START ---
+        $debugPos = mb_strpos($text, 'Куплю');
+        if ($debugPos !== false) {
+            $bytePos = strlen(mb_substr($text, 0, $debugPos));
+            $before  = substr($text, max(0, $bytePos - 20), 20);
+            logger()->debug('BEFORE strip | before "Куплю": ' . bin2hex($before));
+        }
+        // --- DEBUG END ---
+
         $text      = preg_replace('/^[ \t]+/mu', '', $text);
+        // --- DEBUG START ---
+        $debugPos2 = mb_strpos($text, 'Куплю');
+        if ($debugPos2 !== false) {
+            $bytePos2 = strlen(mb_substr($text, 0, $debugPos2));
+            $before2  = substr($text, max(0, $bytePos2 - 20), 20);
+            logger()->debug('AFTER strip | before "Куплю": ' . bin2hex($before2));
+        }
+// --- DEBUG END ---
+
         $textLower = mb_strtolower($text);
         $found     = [];
 
